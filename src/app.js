0638cd27789a05fc49d79bed6d7528ee9941e1d2
Agustín Ruiz <143494692+GoldenDrk@users.noreply.github.com>
2023-09-10 23:24:12 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#nuevaExcusa").addEventListener("click", function() {
+    document.querySelector("#excusa").innerHTML = genedorexcusa();
+  });
+};
+
+let genedorexcusa = function() {
+  let subject = ["The dog ", "My grandma ", "His turtle ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "the keys ", "the car "];
+  let where = [
+    "before the class.",
+    "right on time.",
+    "when I finished.",
+    "during my lunch.",
+    "while I was praying."
+  ];
+
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    subject[subjectIndex] +
+    "" +
+    action[actionIndex] +
+    "" +
+    what[whatIndex] +
+    "" +
+    where[whereIndex]
+  );
 };
